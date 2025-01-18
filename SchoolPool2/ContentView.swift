84b996cf61a2fabe88931779cf6fa467214a03fc
mati22124 <mayanktiku04@gmail.com>
2025-01-18 18:21:04 -0500

    Initial Commit

---
+++ SchoolPool2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SchoolPool2
+//
+//  Created by Mayank Tiku on 1/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SchoolPool2/SchoolPool2App.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolPool2App.swift
+//  SchoolPool2
+//
+//  Created by Mayank Tiku on 1/18/25.
+//
+
+import SwiftUI
+
+@main
+struct SchoolPool2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
